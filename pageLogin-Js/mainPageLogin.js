f98f446c271b82169bb144700a4aa0880981167b
WelllGss <wellingtongss9408@gmail.com>
2022-11-26 22:07:50 -0300

    Style Page-Login

--- pageLogin-Js/mainPageLogin.js
+++ pageLogin-Js/mainPageLogin.js
@@ -3,8 +3,22 @@ import * as firebase from "../index-Js/firebase.js";
 firebase.auth.onAuthStateChanged((user) => {
     if(user) {
         var uid = user.uid;
-        console.log(uid)
+        firebase.db.collection(firebase.COLLECTION).doc(uid).get()
+         .then((user) => {
+                let dataUser = user.data();
+                createHeaderPageLogin(dataUser);
+         })
+         .catch(err => {
+                let errMensage = err.mensage;
+                let errCode = err.code;
+
+                console.log(errMensage, errCode);
+         })
     } else {
         console.log("Nenhum usuário logado")
     }
 })
+
+function createHeaderPageLogin(dataOfUser) {
+    
+}
